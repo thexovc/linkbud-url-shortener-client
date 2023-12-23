b2893a6ac85e2109ac991dd718dfcc3a9ad57de2
thexovc <osazeepeter79@gmail.com>
2023-12-23 11:27:52 +0100

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
 import Navbar from "@/components/Home/Navbar";
 import Main from "@/components/Home/Main";
+import Footer from "@/components/Home/Footer";
 
 export const metadata: Metadata = {
   title: "LinkBud",
@@ -11,6 +12,7 @@ export default function Home() {
     <div className="w-full bg-[#0B101B] min-h-screen flex flex-col items-center">
       <Navbar />
       <Main />
+      <Footer />
     </div>
   );
 }
--- components/Home/Main/index.tsx
+++ components/Home/Main/index.tsx
@@ -2,12 +2,12 @@ import { FaLink } from "react-icons/fa6";
 
 export default function index() {
   return (
-    <div className="w-[95%] bg-[#0B101B]  flex flex-col gap-5 justify-center items-center text-white my-16 md:my-20">
+    <div className="flex-1 w-[95%] bg-[#0B101B]  flex flex-col gap-5 justify-center items-center text-white">
       <h1 className="text-3xl md:text-6xl font-bold text-center">
         Short Links, <span className="text-[#EB568E]">Big Impact.</span>{" "}
       </h1>
-      <p className="w-3/4 md:w-1/3 text-center text-[#C9CED6]">
-        Linkly is an efficient and easy-to-use URL shortening service that
+      <p className="w-5/6 md:w-1/3 text-center text-[#C9CED6]">
+        LinkBud is an efficient and easy-to-use URL shortening service that
         streamlines your online experience.
       </p>
 
@@ -15,7 +15,7 @@ export default function index() {
         <div className="flex gap-2 md:gap-1 items-center">
           <FaLink className=" text-lg md:text-2xl" />
           <input
-            className="bg-transparent placeholder:text-[#C9CED6] outline-none md:text-lg px-0 md:px-6"
+            className="bg-transparent placeholder:text-[#C9CED6] outline-none md:text-lg px-0 md:px-4"
             placeholder="Enter the link here"
             type="text"
           />
